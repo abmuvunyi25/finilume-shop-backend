61fc1935b3d772696ce5f14aab73be9cab542b01
abmuvunyi <ab.muvunyi@gmail.com>
2025-11-19 07:05:15 +0200

    fixing appmodule and packageJson

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,15 @@ import { Product } from './products/entities/product.entity';
 import { Cart } from './cart/entities/cart.entity';
 import { Merchant } from './merchants/entities/merchant.entity';
 import { Listing } from './listings/entities/listing.entity';
+import { Order } from './orders/entities/order.entity';
+import { OrderItem } from './orders/entities/order-item.entity';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
     url: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false },
-    entities: [Product, Cart, Merchant, Listing],
-    migrations: ['src/migrations/*.ts'],
+    entities: [Product, Cart, Merchant, Listing, Order, OrderItem],
+    migrations: ['dist/migrations/*.js'],
     synchronize: false,
-});
+  });
+  
